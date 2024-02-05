1fd5d2bff347e2f0cd464c9ca6241ba5c83c69c9
JongKwonn <133636585+JongKwonn@users.noreply.github.com>
2024-02-05 17:41:28 +0900

    UserStorage 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-
+const UserStorage = require("../../models/UserStorage");
 
 const output ={
   home: (req, res)=>{
@@ -11,28 +11,25 @@ const output ={
   }  
 }
 
-const users ={
-  id : ["woorimIT", "나개발", "김팀장" ],
-  psword : ["1234", "1234", "123456"],
-};
+
 
 const process ={
   login : (req, res) =>{
     const id = req.body.id,
       psword = req.body.psword;
 
-    if(users.id.includes(id)){
-      const idx = users.id.indexOf(id);
-      if (users.psword[idx] === psword){
-        return res.json({
-          success: true,
-        });
-      }
-    }
-    return res.json({
-      successs: false,
-      msg: "로그인에 실패하셨습니다.",
-    })
+    const users = UserStorage.getUsers("id", "psword");
+    // const response = {};
+    // if(users.id.includes(id)){
+    //   const idx = users.id.indexOf(id);
+    //   if (users.psword[idx] === psword){
+    //     response.success = true;
+    //     return res.json(response);
+    //   }
+    // }
+    // response.success = false;
+    // response.msg = "로그인에 실패하셨습니다."
+    // return res.json(response)
   },
 }
 
